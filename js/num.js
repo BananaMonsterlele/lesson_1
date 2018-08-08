fa240f095d6685ad5f02db06ea92d5737ba3d9f4
Aleksey <crazy_frog1234@mail.ru>
2018-08-08 13:17:56 +0300

    second commit

---
+++ js/num.js
@@ -0,0 +1,12 @@
+let num = 33721;
+let multiply = (''+ num).split('');
+let secondMultiply= multiply[0] * multiply[1] * multiply[2] * multiply[3] * multiply[4];
+
+console.log(secondMultiply);
+
+let grade = secondMultiply ** 3;
+let lastStage = (''+ grade).split('');
+
+alert(lastStage[0] + lastStage[1]);
+
+
