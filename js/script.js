43fcc08c5405a959866f64ec57b83e77666ce7c1
Aleksey <crazy_frog1234@mail.ru>
2018-08-10 13:35:31 +0300

    lesson_3

--- js/script.js
+++ js/script.js
@@ -1,27 +1,45 @@
-let monthBudget = +prompt("Ваш бюджет на месяц", "50000");
-let shopName = prompt("Название вашего магазина", "FunnyShop");
-let time = 19;
+let monthBudget,
+    shopName,
+    time,
+    price;   
+
+function start () {
+    monthBudget = prompt("Ваш бюджет на месяц", "50000");
+
+    while(isNaN(monthBudget) ||  monthBudget == '' || monthBudget == null) {
+        monthBudget = prompt("Ваш бюджет на месяц", "50000");
+   }
+
+    shopName = prompt("Название вашего магазина", "FunnyShop").toUpperCase();    
+    time = 19; 
+}
+
+start();
 
 let mainList = {
     monthBudget: monthBudget,
     shopName: shopName,
     shopGoods: [],
     employers: {},
-    open: false
+    open: false,
+    discount: false
 };
 
-for (let i = 0; i < 5; i++) {
+function chooseGoods() {
+    for (let i = 0; i < 5; i++) {
 
-    let a = prompt("Какой тип товаров будем продавать?");
-    
-    if ((typeof(a)) === 'string' && a != '' && a.length < 50) {
-        console.log('All is okey');
-        mainList.shopGoods[i] = a;
-    }  else {
-        alert('Вы не ввели тип товара');
-        i--;
+        let a = prompt("Какой тип товаров будем продавать?");
+        
+        if ((typeof(a)) === 'string' && typeof(a) != null && a != '' && a.length < 50) {
+            console.log('All is okey');
+            mainList.shopGoods[i] = a;
+        }  else {
+            i--;
+        }
     }
 }
+
+chooseGoods();
 
 // let i = 0;
 // while (i < 5) {
@@ -41,17 +59,49 @@ for (let i = 0; i < 5; i++) {
 //     }
 //     i++;
 // } while(i < 5);
+function workTime(time) {
+    if (time < 0) {
+        console.log('Impossible');
+    } else if (time > 8 && time < 20) {
+        console.log('Time to work');
+        } else if (time < 24) {
+            console.log('Its too late');
+            // } else {
+                console.log('There are only 24 hours in a day!');
+            }
+}        
+workTime(18);
 
-if (time < 0) {
-    console.log('Impossible');
-} else if (time > 8 && time < 20) {
-    console.log('Time to work');
-    } else if (time < 24) {
-        console.log('Its too late');
-        } else {
-            console.log('There are only 24 hours in a day!');
-        }
+
+function discountSystem (discount) {
+    if (discount == true) {
+        price = price * 0.8;
+        console.log(price);
+    } 
+}
+
+discountSystem(true);
 
 console.log(mainList);
 
-alert("Дневной бюджет составляет " + monthBudget/30);
+function dayBudget () {
+    alert("Дневной бюджет составляет " + monthBudget/30);
+}
+
+dayBudget();
+
+
+function employers () {
+        let nameOne = prompt('Введите имя первого сотрудника');
+        let nameTwo = prompt('Введите имя второго сотрудника');
+        let nameThree = prompt('Введите имя третьего сотрудника');
+        let nameFour = prompt('Введите имя четвертого сотрудника');
+        mainList.employers = {
+            1: nameOne,
+            2: nameTwo,
+            3: nameThree,
+            4: nameFour
+        };
+    console.log(mainList.employers);
+}
+employers();
---
+++ secodanry_task_lesson_3/script.js
@@ -0,0 +1,9 @@
+let str = 'урок-3-был слишком легким';
+
+console.log(str[0].toUpperCase() + str.slice(1)); 
+
+
+
+let newStr = console.log(str.replace(/-/g, ' '));
+
+
